297893ca48a9fd877caff074f15ea5da4661e054
Guilherme <guilherme.de.medeiros@aptiv.com>
2024-11-14 12:28:58 -0300

    Inserção do modal de Adicionar Filme

---
+++ app/src/main/java/com/example/cinetalk/DialogAdicionar.java
@@ -0,0 +1,42 @@
+package com.example.cinetalk;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import org.jetbrains.annotations.Nullable;
+
+public class DialogAdicionar extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_adicionar, container, false);
+
+        Button confirmButton = view.findViewById(R.id.buttonConfirm);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+    }
+
+}
--- app/src/main/java/com/example/cinetalk/HomeActivity.java
+++ app/src/main/java/com/example/cinetalk/HomeActivity.java
@@ -1,23 +1,32 @@
 package com.example.cinetalk;
 
-import android.app.ActivityOptions;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.ActivityOptionsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FloatingActionButton btnAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.home_activity);
+
+        btnAction = findViewById(R.id.btnAdicionarFilme);
+
+        btnAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogAdicionar dialogAdicionar = new DialogAdicionar();
+                dialogAdicionar.show(getSupportFragmentManager(), "customDialog");
+            }
+        });
     }
 }
